32297eb0bd9b6de55f541656d20ad4a33f2de0d9
Muhammad Talha Rehman <miantalhapaf@gmail.com>
2025-09-30 18:09:35 +0500

    Poppins weight desc

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ const inter = Inter({
 const poppins = Poppins({
   variable: "--font-poppins",
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  weight: [ "400", "500", "600", "700", "800", "900"],
   display: "swap",
 });
 const roboto = Roboto({
